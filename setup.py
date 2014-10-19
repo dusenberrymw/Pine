1e39df01867891ce74387b72760423f0d0e36d9d
Mike Dusenberry <dusenberrymw@gmail.com>
2014-10-19 16:15:30 -0400

    added setup.py to allow installation of pine lib globally for reference in other projects

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='Pine',
+      description='Python Neural Networks',
+      author='Mike Dusenberry',
+      url='https://github.com/dusenberrymw/Pine',
+      packages=['pine'],
+     )
